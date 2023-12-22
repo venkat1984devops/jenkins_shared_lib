07847c7dfbad551edaec53d8536da1725618c9bb
venkat1984devops <142361061+venkat1984devops@users.noreply.github.com>
2023-12-22 15:00:13 -0500

    Update dockerImageCleanup.groovy

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -2,7 +2,8 @@ def call(String project, String ImageTag, String hubUser){
     
      sh """
       docker rmi ${hubUser}/${project}:${ImageTag}
-      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+      docker images
+      //docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
      """
 }
 
