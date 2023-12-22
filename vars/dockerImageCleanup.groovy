dc06f60623af04dd922cd558d8905c8eb21ffe6c
venkat1984devops <142361061+venkat1984devops@users.noreply.github.com>
2023-12-22 15:08:41 -0500

    Update dockerImageCleanup.groovy

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -3,13 +3,13 @@ def call(String project, String ImageTag, String hubUser){
      sh """
       docker images
       docker rmi ${hubUser}/${project}:${ImageTag}
-      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+    
      """
 }
 
 
 //def call(String aws_account_id, String region, String ecr_repoName){
-    
+    //  docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
   //  sh """
     // docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     //"""
